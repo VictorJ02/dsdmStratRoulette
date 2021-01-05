75f9a5d689281c8f300a1fd0335b57ae24895135
Victor Jacobsen <37489922+VictorJ02@users.noreply.github.com>
2021-01-05 15:57:43 +0100

    Added and changed Array

--- strats.js
+++ strats.js
@@ -1,10 +1,16 @@
-let ctStratArray = [
-    'Alle dræp Magnus',
-    'Bli på spawn helt te 30 sekunder'
+let generalCTStratArray = [
+    {heading: "Alle dræp Magnus", strat: "Alle må forsøk å dræp Magnus den hær runden"},
+    {heading: "30 Sec", strat: "Alle må bli på spawn te timer si 30"},
 ];
 
-let tStratArray = [
+let generalTStratArray = [
     'Test 1',
     'Test 2',
     'Maccer!'
 ];
+
+let dust2CTStratArray = 
+
+let dust2TStratArray =
+
+let infernoCTStratArray
