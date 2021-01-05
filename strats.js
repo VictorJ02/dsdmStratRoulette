e759c7f5810ff711b66c7cdfc72c0993b3285e30
Nosum <noahgodhavn@gmail.com>
2021-01-05 19:18:09 +0100

    Update strats.js

--- strats.js
+++ strats.js
@@ -1,16 +1,92 @@
+
 let generalCTStratArray = [
+    {heading: "monnjfull", strat: "Alle må fyll munnen med drikke, og kan ikke svelge/spytte ut før du er død eller runden er ferdig"},
     {heading: "Alle dræp Magnus", strat: "Alle må forsøk å dræp Magnus den hær runden"},
+    {heading: "Bytt hender", strat: "Bytt hånd på tastatur og mus"},
+    {heading: "Nægev eller shniper", strat: "Alle droppe våpenan sine. Den som e nederst på leaderboardet kjøpe en Negev, den som e nerst nederst kjøpe en Scout. Når dem dør kan man plokk opp demmers våpen"},
+    //herifra og ned e antiterrorist unikt
     {heading: "30 Sec", strat: "Alle må bli på spawn te timer si 30"},
 ];
 
 let generalTStratArray = [
-    'Test 1',
-    'Test 2',
-    'Maccer!'
+    {heading: "Monnjfull", strat: "Alle må fyll munnen med drikke, og kan ikke svelge/spytte ut før du er død eller runden er ferdig"},
+    {heading: "Alle dræp Magnus", strat: "Alle må forsøk å dræp Magnus den hær runden"},
+    {heading: "Bytt hender", strat: "Bytt hånd på tastatur og mus"},
+    {heading: "Nægev eller shniper", strat: "Alle droppe våpenan sine. Den som e nederst på leaderboardet kjøpe en Negev, den som e nerst nederst kjøpe en Scout. Når dem dør kan man plokk opp demmers våpen"},
+    //herifra og ned e terrorist unikt
+    {heading: "Headass", strat: "Får du et headshot kan du ikke gjør nokka resten av runden"},
+    {heading: "Die hard", strat: "Kun crouching og Duel Berrettas"},
+    {heading: "", strat: ""},
+    {heading: "", strat: ""},
+    {heading: "", strat: ""},
+    {heading: "", strat: ""},
+    {heading: "", strat: ""},
+    {heading: "", strat: ""},
+
+
 ];
 
-let dust2CTStratArray = 
+    //dust 2 map
+let dust2CTStratArray =
 
 let dust2TStratArray =
 
-let infernoCTStratArray
+
+    //inferno map
+let infernoCTStratArray =
+
+let infernoTStratArray =
+
+
+    //nuke map
+let nukeCTStratArray =
+
+let nukeTStratArray =
+
+
+    //train map
+let trainCTStratArray =
+
+let trainTStratArray =
+
+
+    //mirage map
+let mirageCTStratArray =
+
+let mirageTStratArray =
+
+
+    //vertigo map
+let vertigoCTStratArray =
+
+let vertigoTStratArray =
+
+
+    //overpass map
+let overpassCTStratArray =
+
+let overpassTStratArray =
+
+
+    //agency map
+let agencyCTStratArray =
+
+let agencyTStratArray =
+
+
+    //office map
+let officeCTStratArray =
+
+let officeTStratArray =
+
+
+    //cache map
+let cacheCTStratArray =
+
+let cacheTStratArray =
+
+
+    //anubis map
+let anubisCTStratArray =
+
+let anubisTStratArray =
