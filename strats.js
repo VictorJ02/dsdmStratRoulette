fc472bacfa422b9a8117f22da3a6f41db2a41f2a
Nosum <noahgodhavn@gmail.com>
2021-01-05 20:07:29 +0100

    Update strats.js

--- strats.js
+++ strats.js
@@ -1,26 +1,45 @@
 
 let generalCTStratArray = [
-    {heading: "monnjfull", strat: "Alle må fyll munnen med drikke, og kan ikke svelge/spytte ut før du er død eller runden er ferdig"},
-    {heading: "Alle dræp Magnus", strat: "Alle må forsøk å dræp Magnus den hær runden"},
-    {heading: "Bytt hender", strat: "Bytt hånd på tastatur og mus"},
-    {heading: "Nægev eller shniper", strat: "Alle droppe våpenan sine. Den som e nederst på leaderboardet kjøpe en Negev, den som e nerst nederst kjøpe en Scout. Når dem dør kan man plokk opp demmers våpen"},
+    {heading: "Monnjfull", strat: "Alle må fyll munnen med drikke, og kan ikke svelge/spytte ut før du er død eller runden er ferdig."},
+    {heading: "Alle dræp Magnus", strat: "Alle må forsøk å dræp Magnus den hær runden."},
+    {heading: "Bytt hender", strat: "Bytt hånd på tastatur og mus."},
+    {heading: "Nægev eller shniper", strat: "Alle droppe våpenan sine. Den som e nederst på leaderboardet kjøpe en Negev, den som e nerst nederst kjøpe en Scout. Når dem dør kan man plokk opp demmers våpen."},
+    {heading: "Dårlig kommunikasjon", strat: "Kan bare bann eller si A eller B."},
+    {heading: "Pistol først", strat: "må få en kill med pistolen før du kan bruk hovedvåpenet."},
+    {heading: "Pæng e lættis", strat: "Spilleren med mest pæng må kjøp ting t alle. Resten kan ikke kjøp nokka i det hele tatt."},
+    {heading: "Riflemen", strat: "Laget må ha en av hver rifle, ingen kan ha den samme."},
+    {heading: "BIG FLEX", strat: "Når du drep nonn må du type navnet på den du drept sammen med 'get owned' i chatten. Det må du gjør med én gang."},
+    {heading: "DUBSTEP", strat: "En på laget har voiechat konstant på mens han blæste dubastep på en høytaler gjennom hele runden."},
+    {heading: "Fattiggris", strat: "Man kan kun bruk våpen man ikke har skin på."},
     //herifra og ned e antiterrorist unikt
-    {heading: "30 Sec", strat: "Alle må bli på spawn te timer si 30"},
+    {heading: "30 Sec", strat: "Alle må bli på spawn te timer si 30."},
+    {heading: "Da Hood", strat: "Før runden starte må alle skriv 'finna shoot you up' og så kjøp UMP-45. Alle ska kjør 'drive-bys' uten og stop."},
+    {heading: "Two man army", strat: "Kun to kan vær ut av spawn om gangen. Flere kan går ut når dem dør."},
+    {heading: "Jarand", strat: "Dem 3 øverste på leaderboardet prøve sitt beste å vinn runden, mens dem 2 nederste prøve å hindre dem så my som mulig uten å skad dem."},
+    {heading: "Rap god", strat: "Alle callouts må rappes."},
+    {heading: "Big smoke", strat: "Alle stacke A og gjømme sæ i en smoke fram til bomben e planta"},
 ];
 
 let generalTStratArray = [
-    {heading: "Monnjfull", strat: "Alle må fyll munnen med drikke, og kan ikke svelge/spytte ut før du er død eller runden er ferdig"},
-    {heading: "Alle dræp Magnus", strat: "Alle må forsøk å dræp Magnus den hær runden"},
-    {heading: "Bytt hender", strat: "Bytt hånd på tastatur og mus"},
-    {heading: "Nægev eller shniper", strat: "Alle droppe våpenan sine. Den som e nederst på leaderboardet kjøpe en Negev, den som e nerst nederst kjøpe en Scout. Når dem dør kan man plokk opp demmers våpen"},
+    {heading: "Monnjfull", strat: "Alle må fyll munnen med drikke, og kan ikke svelge/spytte ut før du er død eller runden er ferdig."},
+    {heading: "Alle dræp Magnus", strat: "Alle må forsøk å dræp Magnus den hær runden."},
+    {heading: "Bytt hender", strat: "Bytt hånd på tastatur og mus."},
+    {heading: "Nægev eller shniper", strat: "Alle droppe våpenan sine. Den som e nederst på leaderboardet kjøpe en Negev, den som e nerst nederst kjøpe en Scout. Når dem dør kan man plokk opp demmers våpen."},
+    {heading: "Dårlig kommunikasjon", strat: "Kan bare bann eller si A eller B."},
+    {heading: "Pistol først", strat: "må få en kill med pistolen før du kan bruk hovedvåpenet."},
+    {heading: "Pæng e lættis", strat: "Spilleren med mest pæng må kjøp ting t alle. Resten kan ikke kjøp nokka i det hele tatt."},
+    {heading: "Riflemen", strat: "Laget må ha en av hver rifle, ingen kan ha den samme."},
+    {heading: "BIG FLEX", strat: "Når du drep nonn må du type navnet på den du drept sammen med 'get owned' i chatten. Det må du gjør med én gang."},
+    {heading: "DUBSTEP", strat: "En på laget har voiechat konstant på mens han blæste dubastep på en høytaler gjennom hele runden."},
+    {heading: "You spin me right round", strat: "Hver gang man får en kill må man snurr en runde på stolen og rop WEEEEEEE!!!"},
     //herifra og ned e terrorist unikt
-    {heading: "Headass", strat: "Får du et headshot kan du ikke gjør nokka resten av runden"},
-    {heading: "Die hard", strat: "Kun crouching og Duel Berrettas"},
-    {heading: "", strat: ""},
-    {heading: "", strat: ""},
-    {heading: "", strat: ""},
-    {heading: "", strat: ""},
-    {heading: "", strat: ""},
+    {heading: "Headass", strat: "Får du et headshot kan du ikke gjør nokka resten av runden."},
+    {heading: "Die hard", strat: "Kun crouching og Duel Berrettas."},
+    {heading: "Bangarang", strat: "Rush B, så CT spawn, og så plante en plass."},
+    {heading: "Tog", strat: "Alle rushe til det tetteste chocepointet på banen, og så går vi inn en etter en."},
+    {heading: "Fryst bombe", strat: "Den som har bomben kan ikke beveg sæ."},
+    {heading: "Maccern", strat: "Alle kjøpe MAC-10 og rushe en av bombeområdan."},
+    {heading: "Lurt å plante", strat: "Hvis bomben ikke blir planta den her runden, må alle ha +right i konsollen neste runde. Det her blir i tillegg til neste strat."},
     {heading: "", strat: ""},
 
 
@@ -72,15 +91,24 @@ let overpassTStratArray =
 
 
     //agency map
-let agencyCTStratArray =
+let agencyCTStratArray = [
+    {heading: "Tony Hawk", strat: "Hver gang du ser en rail (gellender) må du gå opp på den og rop masse skate-triks. Når du går av må du ta en 360."}
+]
 
-let agencyTStratArray =
+let agencyTStratArray = [
+    {heading: "Tony Hawk", strat: "Hver gang du ser en rail (gellender) må du gå opp på den og rop masse skate-triks. Når du går av må du ta en 360."}
+]
 
 
     //office map
-let officeCTStratArray =
+let officeCTStratArray = [
+    {heading: "Rainbow 6 siege", strat: "Alle må gå inn stigen til vinduet til kontoret og snik sæ bort mot motsatnderan."}
+    {heading: "Guerilla", strat: "Alle går til det store vinduet bak garasjen croucha, og reise sæ samtidig."}
+]
 
-let officeTStratArray =
+let officeTStratArray = [
+    {heading: "Se, æ e en busk!", strat: "Alle må sitt i en busk / plante."}
+]
 
 
     //cache map
