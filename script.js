b11752aeb37ff147e75ee09bad1ca447d9b1f9e2
Victor Jacobsen <37489922+VictorJ02@users.noreply.github.com>
2021-01-05 15:40:57 +0100

    Changed Arrays

--- script.js
+++ script.js
@@ -2,17 +2,6 @@ let ctButton = document.getElementById('ctButton');
 let tButton = document.getElementById('tButton');
 let stratAnswer = document.getElementById('stratAnswer');
 
-let ctStratArray = [
-    'Alle dræp Magnus',
-    'Bli på spawn helt te 30 sekunder'
-];
-
-let tStratArray = [
-    'Test 1',
-    'Test 2',
-    'Maccer!'
-];
-
 ctButton.addEventListener("click", ctStrat);
 tButton.addEventListener("click", tStrat);
 
---
+++ strats.js
@@ -0,0 +1,10 @@
+let ctStratArray = [
+    'Alle dræp Magnus',
+    'Bli på spawn helt te 30 sekunder'
+];
+
+let tStratArray = [
+    'Test 1',
+    'Test 2',
+    'Maccer!'
+];
