d320e4a61b29e48f832847175bcb98739480ad3f
sigavh <77015240+sigavh@users.noreply.github.com>
2021-01-12 20:52:02 +0100

    oj sann

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ function mapChooser(map) {
     ctStrats = generalCTStratArray.concat(mapCTStratArray[map]);
     tStrats = generalTStratArray.concat(mapTStratArray[map]);
     console.log(ctStrats, tStrats);
-    buttonsEl.innerHTML = '<button type="button" id="ctButton" onclick="ctStrat()">CT</button><button type="button" id="tButton" onclick="tStrat()">T</button><form method="get" action="javascript:var%20KICKASSVERSION='2.0';var%20s%20=%20document.createElement('script');s.type='text/javascript';document.body.appendChild(s);s.src='//hi.kickassapp.com/kickass.js';void(0);"><button type="submit">sex</button></form>';
+    buttonsEl.innerHTML = '<button type="button" id="ctButton" onclick="ctStrat()">CT</button><button type="button" id="tButton" onclick="tStrat()">T</button><form method="get" action="javascript:var%20KICKASSVERSION="2.0";var%20s%20=%20document.createElement("script");s.type="text/javascript";document.body.appendChild(s);s.src="//hi.kickassapp.com/kickass.js";void(0);"><button type="submit">sex</button></form>';
 }
 
 function ctStrat() {
