5e493c39510878ac95c03c4ba4835775dfbc3b8f
sigavh <77015240+sigavh@users.noreply.github.com>
2021-01-12 20:17:50 +0100

    Update script.js

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ function mapChooser(map) {
     ctStrats = generalCTStratArray.concat(mapCTStratArray[map]);
     tStrats = generalTStratArray.concat(mapTStratArray[map]);
     console.log(ctStrats, tStrats);
-    buttonsEl.innerHTML = '<button type="button" id="ctButton" onclick="ctStrat()">CT</button><button type="button" id="tButton" onclick="tStrat()">T</button><form method="get" action="index.html"><button type="submit">Tilbake</button></form>';
+    buttonsEl.innerHTML = '<button type="button" id="ctButton" onclick="ctStrat()">CT</button><button type="button" id="tButton" onclick="tStrat()">T</button><form method="get" action="https://victorj02.github.io/dsdmStratRoulette"><button type="submit">Tilbake</button></form>';
 }
 
 function ctStrat() {
