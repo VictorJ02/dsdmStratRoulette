fd9e01539521a41e0d8e15b0977ff392874c2bb0
Victor Jacobsen <37489922+VictorJ02@users.noreply.github.com>
2021-01-13 10:59:38 +0100

    Masse drit

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,17 @@
 let buttonsEl = document.getElementById('buttons');
 let stratHeadingEl = document.getElementById('stratHeading');
 let stratAnswerEl = document.getElementById('stratAnswer');
+let head = document.getElementsByTagName('HEAD')[0];
+let linkStratDisplay = document.createElement('link');
+let linkTeamSelect = document.createElement('link');
+
+linkTeamSelect.rel = 'stylesheet';  
+linkTeamSelect.type = 'text/css'; 
+linkTeamSelect.href = 'stylesheets/styleTeamSelect.css'; 
+
+linkStratDisplay.rel = 'stylesheet';  
+linkStratDisplay.type = 'text/css'; 
+linkStratDisplay.href = 'stylesheets/styleStratDisplay.css';  
 
 let ctStrats = "";
 let tStrats = "";
@@ -9,17 +20,26 @@ function mapChooser(map) {
     ctStrats = generalCTStratArray.concat(mapCTStratArray[map]);
     tStrats = generalTStratArray.concat(mapTStratArray[map]);
     buttonsEl.innerHTML = '<img src="media/images/ct.png" id="ctButton" onclick="ctStrat()"></img><img src="media/images/t.png" id="tButton" onclick="tStrat()"></img>';
+
+    head.appendChild(linkTeamSelect);
 }
 
 function ctStrat() {
     let randomStrat = Math.floor(ctStrats.length * Math.random());
-    
+    let styleRemove = document.querySelector('link');
+
     if (ctStrats[randomStrat].heading == stratHeadingEl.innerHTML) {
         ctStrat();
     };
 
+    if (styleRemove == '<link rel="stylesheet" type="text/css" href="stylesheets/styleTeamSelect.css">') {
+        head.removeChild(linkTeamSelect);
+    }
+
     stratHeadingEl.innerHTML = ctStrats[randomStrat].heading;
-    stratAnswerEl.innerHTML = ctStrats[randomStrat].strat;
+    stratAnswerEl.innerHTML = ctStrats[randomStrat].strat; 
+
+    head.appendChild(linkStratDisplay); 
 }
 
 function tStrat() {
@@ -32,3 +52,5 @@ function tStrat() {
     stratHeadingEl.innerHTML = tStrats[randomStrat].heading;
     stratAnswer.innerHTML = tStrats[randomStrat].strat;
 }
+
+let styleRemove = document.querySelector('link');
